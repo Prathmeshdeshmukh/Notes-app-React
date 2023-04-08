fdc2b81913fe302aa96299662d2105497220b179
Prathmeshdeshmukh <87267186+Prathmeshdeshmukh@users.noreply.github.com>
2023-04-08 09:34:57 +0530

    done with basic react setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Split from "react-split"
+import {nanoid} from "nanoid"
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Split 
+          sizes = {[30,70]}
+          direction= "horizontal"
+          className= "split"
+      >
+          <Sidebar />
+          <Editor />
+
+      </Split>
+    </main>
   );
 }
 
